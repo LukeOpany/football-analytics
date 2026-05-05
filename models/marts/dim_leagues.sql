896e238b1ad69d8de5725210fd888dc797923e02
Luke Opany <brianopany@gmail.com>
2026-05-05 06:35:08 +0300

    Join league tier seed into dim_leagues

--- models/marts/dim_leagues.sql
+++ models/marts/dim_leagues.sql
@@ -6,6 +6,10 @@ countries as (
     select * from {{ ref('stg_countries') }}
 ),
 
+league_tiers as (
+    select * from {{ ref('league_tiers') }}
+),
+
 match_stats as (
     select
         league_name,
@@ -22,12 +26,18 @@ joined as (
         l.league_id,
         l.league_name,
         c.country_name,
+        lt.tier,
+        lt.confederation,
+        lt.is_top_division,
         ms.total_matches,
         ms.total_seasons,
         round(ms.avg_goals_per_match::numeric, 2) as avg_goals_per_match,
         ms.total_goals
     from leagues l
     left join countries c  on l.country_id = c.country_id
+    left join league_tiers lt
+        on l.league_name = lt.league_name
+        and c.country_name = lt.country_name
     left join match_stats ms on l.league_name = ms.league_name
 )
 
