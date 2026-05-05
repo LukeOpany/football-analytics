57fb5ea832f87c8034aaf8b1422d8606b318ab16
Luke Opany <brianopany@gmail.com>
2026-05-05 08:09:51 +0300

    add incremental loading to stg_matches

--- models/staging/stg_matches.sql
+++ models/staging/stg_matches.sql
@@ -1,16 +1,22 @@
+{{ config(materialized='incremental', unique_key='match_id') }}
+
 with source as (
     select * from {{ source('raw', 'match') }}
+
+    {% if is_incremental() %}
+        where date::date > (select max(match_date) from {{ this }})
+    {% endif %}
 ),
 
 renamed as (
     select
-        id                  as match_id,
+        id              as match_id,
         match_api_id,
         country_id,
         league_id,
         season,
         stage,
-        date                ::date as match_date,
+        date::date      as match_date,
         home_team_api_id,
         away_team_api_id,
         home_team_goal,
